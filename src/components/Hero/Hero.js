240db78ac4f5de625709865c72d8dd9225ad603e
ljv29 <jumpswervelogan@gmail.com>
2022-06-16 11:53:30 -0700

    95% done with hero/nav

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -10,16 +10,34 @@ const Hero = () => {
                     <img src={logo} alt="logo" />
                 </div>
                 <div className="nav-items">
-                    <a>ABOUT</a>
-                    <a>PROJECTS</a>
-                    <a>CONTACT</a>
-                    <a  id="resume" href="https://drive.google.com/file/d/1tiXCMvwznPjx71Ui0AOhlyu4aq9BEkrNFeg01pOKaHw/view" target="_blank">Resume</a>
+                    <a id="about-item">ABOUT</a>
+                    <a id="projects-item">PROJECTS</a>
+                    <a id="contact-item">CONTACT</a>
+                    <a id="resume" href="https://drive.google.com/file/d/1tiXCMvwznPjx71Ui0AOhlyu4aq9BEkrNFeg01pOKaHw/view" target="_blank">Resume</a>
                 </div>
+                <div className="nav-items-mobile">
+                    <p id="close-mobile" onClick={() => {
+                        document.querySelector(".nav-menu").classList.toggle("active");
+                        document.querySelector(".nav-items-mobile").classList.toggle("active");
+                    }}>X</p>
+                    <a id="about-mobile">ABOUT</a>
+                    <a id="projects-mobile">PROJECTS</a>
+                    <a id="contact-mobile">CONTACT</a>
+                    <a id="resume-mobile" href="https://drive.google.com/file/d/1tiXCMvwznPjx71Ui0AOhlyu4aq9BEkrNFeg01pOKaHw/view" target="_blank">Resume</a>
+                </div>
+                <button className="nav-menu" onClick={() => {
+                    document.querySelector(".nav-menu").classList.toggle("active");
+                    document.querySelector(".nav-items-mobile").classList.toggle("active");
+                }}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
             </div>
             <div className="hero-info">
                 <div className="socials">
-                    <a href="https://www.linkedin.com/in/logan-veik-018187196/" target="_blank" rel="noreferrer"><i id="social-icon" class="fa-brands fa-linkedin"></i></a>
-                    <a href="https://github.com/loganveik" target="_blank" rel="noreferrer"><i id="social-icon" class="fa-brands fa-github"></i></a>
+                    <a href="https://www.linkedin.com/in/logan-veik-018187196/" target="_blank" rel="noreferrer"><i id="social-icon" className="fa-brands fa-linkedin"></i></a>
+                    <a href="https://github.com/loganveik" target="_blank" rel="noreferrer"><i id="social-icon" className="fa-brands fa-github"></i></a>
                 </div>
                 <div className="fullname">
                     <p>LOGAN VEIK</p>
